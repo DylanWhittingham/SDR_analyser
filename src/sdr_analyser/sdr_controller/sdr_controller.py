d127d075f260b53157cba1ca12ae4e0b7119aa4c
dylanhunter <dylan2.whittingham@live.uwe.ac.uk>
2024-03-05 12:38:50 +0000

    sdr_controller updated with psd graph functionality and basic iq_file logging

--- src/sdr_analyser/sdr_controller/sdr_controller.py
+++ src/sdr_analyser/sdr_controller/sdr_controller.py
@@ -1,11 +1,36 @@
 from rtlsdr import RtlSdr
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import mlab
 
 sdr = RtlSdr()
 
-# configure device
-sdr.sample_rate = 2.048e6  # Hz
-sdr.center_freq = 70e6     # Hz
-sdr.freq_correction = 60   # PPM
-sdr.gain = 'auto'
+sample_rate = 102e4
+freq = 174e6
+gain = 50
 
-print(sdr.read_samples(512))
+
+#SDR Configuration
+sdr.sample_rate = sample_rate  # Msps
+sdr.center_freq = freq # Hz
+sdr.gain = gain
+
+iq_samples = sdr.read_samples(1*sample_rate)
+
+
+
+for x in range(5):
+    np.append(iq_samples, sdr.read_samples(1*sample_rate))
+
+np.save('sdr_iq_data/iq_samples.npy', iq_samples)
+
+loaded_iq = np.load('iq_samples.npy') 
+
+print('Sampling @', sdr.sample_rate, 'Msps')
+print('Frequency @', sdr.center_freq, 'Hz')
+
+plt.psd(loaded_iq, NFFT=1024, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6, detrend='mean')
+
+plt.xlabel('Frequency (MHz)')
+plt.ylabel('Relative power (dB)')
+plt.show()
