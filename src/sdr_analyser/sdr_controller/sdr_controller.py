3d61042d30a726808372707420a9b4d9b61d9e00
dylanhunter <dylan2.whittingham@live.uwe.ac.uk>
2024-03-18 13:08:07 +0000

    added meta-data functionality and updated to SigMF standard

--- src/sdr_analyser/sdr_controller/sdr_controller.py
+++ src/sdr_analyser/sdr_controller/sdr_controller.py
@@ -2,37 +2,64 @@ from rtlsdr import RtlSdr
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import mlab
+import sigmf
+import os
+from sigmf import SigMFFile
+import datetime as dt
+from sigmf.utils import get_data_type_str
+
 
 sdr = RtlSdr()
 
-sample_rate = 102e4
+activeFile = 'sdr_iq_data/iq_samples.sigmf-data'
+
+sampleRate = 102e4
 freq = 174e6
 gain = 50
-
+sampleTime = 5
+signalDuration = sampleTime / sdr.sample_rate
+currentTime = dt.datetime.utcnow().isoformat()+'Z' #ZULU time
 
 #SDR Configuration
-sdr.sample_rate = sample_rate  # Msps
+sdr.sample_rate = sampleRate  # Msps
 sdr.center_freq = freq # Hz
-sdr.gain = gain
+sdr.gain = gain #dB
 
-iq_samples = sdr.read_samples(1*sample_rate)
+iqSamples = sdr.read_samples(1*sampleRate)
 
+for x in range(sampleTime):
+    np.append(iqSamples, sdr.read_samples(1*sampleRate))
 
+iqSamples = iqSamples.astype(np.complex64)
 
-for x in range(5):
-    np.append(iq_samples, sdr.read_samples(1*sample_rate))
+iqSamples.tofile(activeFile)
 
-iq_samples = iq_samples.astype(np.complex64)
+meta = SigMFFile(
+    data_file=activeFile, # extension is optional
+    global_info = {
+        SigMFFile.DATATYPE_KEY: get_data_type_str(iqSamples),  # in this case, 'cf32_le'
+        SigMFFile.SAMPLE_RATE_KEY: sdr.sample_rate,
+        SigMFFile.AUTHOR_KEY: 'DylanWhittingham@outlook.com',
+        SigMFFile.DESCRIPTION_KEY: 'RF Recording of an FM radio',
+    }
+)
 
-iq_samples.tofile('sdr_iq_data/iq_samples.iq')
+meta.add_capture(0, metadata={
+    SigMFFile.FREQUENCY_KEY: sdr.center_freq,
+    SigMFFile.DATETIME_KEY: currentTime,
+})
+meta.validate()
+meta.tofile('sdr_iq_data/iq_samples.sigmf-meta') # extension is optional
 
-loaded_iq = np.fromfile('sdr_iq_data/iq_samples.iq', np.complex64)
+loadedIQ = np.fromfile(activeFile, np.complex64)
 
 print('Sampling @', sdr.sample_rate, 'Msps')
 print('Frequency @', sdr.center_freq, 'Hz')
+print('Sampling Length @', signalDuration, 'Seconds')
 
-plt.psd(loaded_iq, NFFT=1024, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6, detrend='mean')
-print(iq_samples)
+plt.psd(loadedIQ, NFFT=1024, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6, detrend='mean')
+plt.spe
+print(iqSamples)
 plt.xlabel('Frequency (MHz)')
 plt.ylabel('Relative power (dB)')
 plt.show()
