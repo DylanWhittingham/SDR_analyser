dc5320b5ea182970add2985807767c33a3f03a61
dylanhunter <dylan2.whittingham@live.uwe.ac.uk>
2024-03-19 21:37:41 +0000

    prerelease SDR Analyser

--- src/sdr_analyser/sdr_controller/new_sdr_controller.py
+++ src/sdr_analyser/sdr_controller/new_sdr_controller.py
@@ -8,30 +8,46 @@ from sigmf.utils import get_data_type_str
 import datetime as dt
 from pylab import *
 from scipy import signal
-activeFile = 'sdr_iq_data/iq_samples.sigmf-data'
+import os.path
+
+
+print('Welcome to SDR Analyser!\n')
+filename = input('Please input the name of your IQ File')
+
+activeSignal = 'sdr_iq_data/'+filename+'.sigmf-data'
+activeMetadata = 'sdr_iq_data/'+filename+'.sigmf-meta'
 sdr = RtlSdr()
 
-##configure SDR
-sampleRate = 2.048e6 
+def get_db(sample):
+    return ((10*np.log10(sample)))
+
+def get_noiseAverage(sample):
+    return int(np.mean(get_db(sample)))
+
+def get_signalPower(sample):
+    return int(np.max(get_db(sample)))
+
+def get_snr(sample):
+    return int(get_db(np.max(sample) / (np.sum(sample) - np.max(sample))))
+
+# configure SDR
+sampleRate = 2.048e6
 freq = 174e6
 gain = 50
-sampleTime = 1
+sampleTime = 5
 signalDuration = sampleTime / sdr.sample_rate
-currentTime = dt.datetime.utcnow().isoformat()+'Z' #ZULU time
+currentTime = dt.datetime.utcnow().isoformat()+'Z'  # ZULU time
 sdr.sample_rate = sampleRate  # Msps
-sdr.center_freq = freq # Hz
-sdr.gain = gain #dB
+sdr.center_freq = freq  # Hz
+sdr.gain = gain  # dB
 
-iqSamples = sdr.read_samples(sampleTime*sampleRate) #make iq
-
-iqSamples = iqSamples.astype(np.complex64)
-
-iqSamples.tofile(activeFile)
+iqSamples = (sdr.read_samples(sampleTime*sampleRate).astype(np.complex64))
+iqSamples.tofile(activeSignal)
 
 meta = SigMFFile(
-    data_file=activeFile, 
-    global_info = {
-        SigMFFile.DATATYPE_KEY: get_data_type_str(iqSamples),  # in this case, 'cf32_le'
+    data_file=activeSignal,
+    global_info={
+        SigMFFile.DATATYPE_KEY: get_data_type_str(iqSamples),
         SigMFFile.SAMPLE_RATE_KEY: sdr.sample_rate,
         SigMFFile.AUTHOR_KEY: 'DylanWhittingham@outlook.com',
         SigMFFile.DESCRIPTION_KEY: 'RF Recording of an FM radio',
@@ -43,10 +59,9 @@ meta.add_capture(0, metadata={
     SigMFFile.DATETIME_KEY: currentTime,
 })
 meta.validate()
-meta.tofile('sdr_iq_data/iq_samples.sigmf-meta') # extension is optional
+meta.tofile(activeMetadata)
 
-
-signal = sigmffile.fromfile(activeFile)
+signal = sigmffile.fromfile(activeSignal)
 samples = signal.read_samples().view(np.complex64).flatten()
 
 # Get some metadata and all annotations
@@ -54,30 +69,19 @@ sample_rate = signal.get_global_field(SigMFFile.SAMPLE_RATE_KEY)
 sample_count = signal.sample_count
 signal_duration = sample_count / sample_rate
 
-rel_power, freq = psd(samples, NFFT=1024, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6, scale_by_freq=False)
 
-rel_power_db = 10*np.log10(rel_power)
-###################
-#end of controller#
-###################
+power, freq = psd(samples, NFFT=1024, Fs=sdr.sample_rate/1e6,
+                  Fc=sdr.center_freq/1e6, scale_by_freq=False)
 
-print(sample_rate)
-print(sample_count)
-print(signal_duration)
+print('Noise Average is ', get_noiseAverage(power), 'dB')
+print('Signal Power is', get_signalPower(power), 'dB')
+print('SNR is ', get_snr(power), 'dB')
 
-print(rel_power_db)
+if get_snr(power)>0:
+    print('Good Signal, above noisefloor')
+else:
+    print('Bad Signal, below noisefloor')
 
-noise = np.mean(rel_power_db)
-signalPow = np.max(rel_power_db)
-
-print(noise)
-
-print(signalPow)
-
-snr = 10 * np.log10(np.max(rel_power) / (np.sum(rel_power) - np.max(rel_power)))
-print(snr)
-# print('freq', freq)
-# print('power', power_db)
 plt.xlabel('Frequency (MHz)')
 plt.ylabel('Relative power (dB)')
 plt.show()
